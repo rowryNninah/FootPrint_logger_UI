de9361b7cbc7fadbce73993f4e2ee38e02d5a758
Rorisang <53812088+rowryNninah@users.noreply.github.com>
2025-07-06 20:40:26 +0000

    created the app

---
+++ script.js
@@ -0,0 +1,12 @@
+const rates = {
+    petrol: 2.3,
+    diesel: 2.5,
+    electricity: 0.9,
+    gas: 2.9,
+    meat: 15.5,
+    dairy: 1.6,
+    fruits: 0.5,
+    veggies: 0.5,
+}
+
+function conversion(input, results, )
